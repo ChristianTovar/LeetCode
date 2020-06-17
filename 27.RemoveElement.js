3651362435856dfd380b72d55c9095c2a225b26e
Christian Tovar <tovarchristian21@gmail.com>
2020-06-17 13:32:40 -0500

    added exercise 27 solution

---
+++ 27.RemoveElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let i = 0;
+  
+  while(nums[i] != undefined) {
+    if(nums[i] === val) {
+      nums.splice(i, 1);
+      continue;
+    }
+    
+    i++;
+  }
+};
+
+
