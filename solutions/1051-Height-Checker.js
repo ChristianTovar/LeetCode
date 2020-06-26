93de992a984494260140cc7a1dbf81edd3112649
Christian Tovar <tovarchristian21@gmail.com>
2020-06-26 14:40:59 -0500

    Add 1051 solution

---
+++ solutions/1051-Height-Checker.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+const heightChecker = function(heights) {
+  let count = 0;
+  const sorted = [...heights];
+  heights.sort((a, b) => a - b);
+ 
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] != sorted[i]) count++;
+  }
+  
+  return count;
+};
+
+
