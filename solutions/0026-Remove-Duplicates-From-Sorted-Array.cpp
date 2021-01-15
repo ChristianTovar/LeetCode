6e26040de3768345f92a29acd0b55be8556878a4
Christian Tovar <tovarchristian21@gmail.com>
2021-01-14 21:36:45 -0500

    Improve 0026 solution

--- solutions/0026-Remove-Duplicates-From-Sorted-Array.cpp
+++ solutions/0026-Remove-Duplicates-From-Sorted-Array.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
   int removeDuplicates(vector<int>& nums) {
-    for (auto it = nums.begin(); it != nums.end(); ++it) {
-      if (it + 1 == nums.end()) break;
-      if (*it == *(it + 1)) {
-        nums.erase(it);
-        it -= 1;
-      }
-    }
-    
-    return nums.size();
+    set<int> s(nums.begin(), nums.end());
+    nums.assign(s.begin(), s.end());
+    return s.size();
   }
 };
