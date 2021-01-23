3480675a09916ab13ed2cfe5b254865d3dfabb8f
Christian Tovar <tovarchristian21@gmail.com>
2021-01-23 15:35:31 -0500

    Add 0136 solution

---
+++ solutions/0136-Single-Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  int singleNumber(vector<int>& nums) {
+    int x = 0;
+    for (auto it = nums.begin(); it != nums.end(); ++it) {
+      x ^= *it;
+    }
+    
+    return x;
+  }
+};
